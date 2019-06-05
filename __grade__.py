aa594a836cd088e351abf3be564b4380a1dc4d27
Ana María Hincapié Berrio <50270277+ahincapieb@users.noreply.github.com>
2019-06-04 21:10:22 -0500

    Update __grade__.py

--- __grade__.py
+++ __grade__.py
@@ -1,7 +1,7 @@
 import glob
 import doctest
 
-result = doctest.testfile('__grade__.txt', report = False, verbose = False)
+result = doctest.testfile('__Grade__.txt', report = False, verbose = False)
 a,b = result
 print('\n>>>>  ', result, ' <<<<\n')
 open('__grade__', 'w').write(str(round(float(b-a) / float(b) * 5.0 , 1)))
